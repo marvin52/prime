0fb672ed2eb2499e94e2d5e9506add1b5b58926d
Marvin Medeiros <marvin.medeiros52@gmail.com>
2024-07-30 20:36:40 -0300

    estudo primos

--- estudoPiPrime.js
+++ estudoPiPrime.js
@@ -3,7 +3,7 @@ const args = process.argv.slice(2);
 const pos = parseInt(args[0]);
 
 function nPrime(n) {
-    n = (n * (41 / 13)) - (13 + ((n + 13) / (41 - 1)));
+    n = (n * (41 / 13)) - (13 + ((n + 13) / (41            )));
     return (n * Math.log(n) / (41 / 13));
 }
 
